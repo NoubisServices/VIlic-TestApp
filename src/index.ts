8d6aeaf8cd222ed1ca06cabd4d26619515336440
Vladimir <vladimir.ilic@noubis.com>
2021-06-15 10:58:27 +0200

    Added Express server working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Welcomen')
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+
+app.listen(PORT, () => {
+  console.log(`TaskApp is running at https://localhost:${PORT}`);
+});
